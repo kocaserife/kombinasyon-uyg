da6d5fbb4f7281b9f3f3034d4e1344b2a74f034f
kocaserife <serifekoca0742@gmail.com>
2022-05-24 07:28:41 +0300

    kombinasyon hesaplama uygulaması
    java ile girilen n ve r değerlerinden kombinasyonunu hesaplama uygulaması

---
+++ kombinasyon/src/kombinasyone.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class kombinasyone {
+    public static void main(String[] args) {
+        int n, r, f1 = 1, f2 = 1, f3 = 1;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("n yaz: ");
+        n = input.nextInt();
+        System.out.print("r' yaz: ");
+        r = input.nextInt();
+        for (int i = 1; i <= n; i++) {
+            f1 = f1 * i;
+        }
+        for (int j = 1; j <= r; j++) {
+            f2 = f2 * j;
+        }
+        for (int k = 1; k <= (n - r); k++) {
+            f3 = f3 * k;
+        }
+        System.out.print("kombinasyonee: ");
+        System.out.println(f1 / (f2 * f3));
+
+
+    }
+}
